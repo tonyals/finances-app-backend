0a30711d7c0c3461bf227fe974d9f859a8419224
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-05-04 20:26:56 -0300

    test: ensure SignUpController returns an account if addAccount success

--- src/presentation/controllers/sign-up.spec.ts
+++ src/presentation/controllers/sign-up.spec.ts
@@ -1,8 +1,10 @@
 import { SignUpController } from './sign-up'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, success } from '../helpers/http-helper'
 import { MissingParamError } from '../errors/missing-param'
 import { InvalidParamError } from '../errors/invalid-param'
 import { EmailValidator } from '../protocols/email-validator'
+import { AddAccount, AddAccountModel } from '../../domain/usecases/add-account'
+import { AccountMondel } from '../../domain/models/account-model'
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
@@ -13,15 +15,32 @@ const makeEmailValidator = (): EmailValidator => {
   return new EmailValidatorStub()
 }
 
+const makeAddAccount = (): AddAccount => {
+  class AddAccountStub implements AddAccount {
+    async addAccount (accountData: AddAccountModel): Promise<AccountMondel> {
+      const fakeAccount = {
+        id: 1,
+        name: 'any_name',
+        email: 'valid_email',
+        password: 'any_password'
+      }
+      return new Promise(resolve => resolve(fakeAccount))
+    }
+  }
+  return new AddAccountStub()
+}
+
 interface SutTypes {
+  addAccountStub: AddAccount
   sut: SignUpController
   emailValidatorStub: EmailValidator
 }
 
 const makeSut = (): SutTypes => {
+  const addAccountStub = makeAddAccount()
   const emailValidatorStub = makeEmailValidator()
-  const sut = new SignUpController(emailValidatorStub)
-  return { emailValidatorStub, sut }
+  const sut = new SignUpController(emailValidatorStub, addAccountStub)
+  return { emailValidatorStub, sut, addAccountStub }
 }
 
 describe('SignUpController', () => {
@@ -99,5 +118,23 @@ describe('SignUpController', () => {
       }
     })
     expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
+  })
+
+  test('should return an account if addAccount success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({
+      body: {
+        name: 'any_name',
+        email: 'invalid_mail@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    })
+    expect(httpResponse).toEqual(success({
+      id: 1,
+      name: 'any_name',
+      email: 'valid_email',
+      password: 'any_password'
+    }))
   })
 })
