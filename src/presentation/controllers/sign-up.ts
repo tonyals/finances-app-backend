31aceee6b18b8bfcce12115f675f36e9692644d5
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-05-04 19:52:31 -0300

    feat: ensure SignUpController returns 400 if password and passwordConfirmation not match

--- src/presentation/controllers/sign-up.ts
+++ src/presentation/controllers/sign-up.ts
@@ -2,17 +2,20 @@ import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { badRequest } from '../helpers/http-helper'
 import { MissingParamError } from '../errors/missing-param'
+import { InvalidParamError } from '../errors/invalid-param'
 
 export class SignUpController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
     }
-
+    const { password, passwordConfirmation } = httpRequest.body
+    if (password !== passwordConfirmation) {
+      return badRequest(new InvalidParamError('passwordConfirmation'))
+    }
     return new Promise(resolve => resolve(null))
   }
 }
