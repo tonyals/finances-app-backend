9c182f35449f9a42eaed9045763e8074a44f3123
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-05-04 14:45:30 -0300

    feat: ensure DbAccount calls Encrypter with correct password

--- src/data/database/postgres/db-account.ts
+++ src/data/database/postgres/db-account.ts
@@ -1,14 +1,17 @@
 import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
 import { AccountMondel } from '../../../domain/models/account-model'
 import { AccountRepository } from '../usecases/account-repository'
+import { Encrypter } from '../usecases/encrypter'
 
 export class DbAccount implements AddAccount {
   constructor (
-    private readonly accountRepository: AccountRepository
+    private readonly accountRepository: AccountRepository,
+    private readonly encrypt: Encrypter
   ) {}
 
   async addAccount (accountData: AddAccountModel): Promise<AccountMondel> {
     await this.accountRepository.addAccountRepository(accountData)
+    await this.encrypt.encrypt(accountData.password)
     return null
   }
 }
