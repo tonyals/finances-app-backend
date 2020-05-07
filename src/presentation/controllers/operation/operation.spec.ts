90ca0ebc8ed00ac50e3f70467219a5288c11bed5
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-05-07 15:31:01 -0300

    test: ensure OperationController returns 400 if no amount is provided

--- src/presentation/controllers/operation/operation.spec.ts
+++ src/presentation/controllers/operation/operation.spec.ts
@@ -1,6 +1,7 @@
 import { OperationController } from './operation'
 import { badRequest } from '../../helpers/http-helper'
 import { MissingParamError } from '../../errors/missing-param'
+import { OperationType } from '../../../domain/models/operation-model'
 
 interface SutTypes {
   sut: OperationController
@@ -22,5 +23,17 @@ describe('OperationController', () => {
       }
     })
     expect(httpResponse).toEqual(badRequest(new MissingParamError('type')))
+  })
+
+  test('should return 400 if no amount is provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({
+      body: {
+        type: OperationType.CREDIT,
+        date: new Date(),
+        description: 'any_description'
+      }
+    })
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('amount')))
   })
 })
