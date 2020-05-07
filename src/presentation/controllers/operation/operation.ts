f2d8f1a9eae01f4891e636ba9b45f3aea807374e
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-05-07 16:01:08 -0300

    feat: ensure OperationController returns 500 if addCreditOperation throws

--- src/presentation/controllers/operation/operation.ts
+++ src/presentation/controllers/operation/operation.ts
@@ -1,21 +1,25 @@
 import { Controller } from '../../protocols/controller'
 import { HttpResponse, HttpRequest } from '../../protocols/http'
 import { MissingParamError } from '../../errors/missing-param'
-import { badRequest, success } from '../../helpers/http-helper'
+import { badRequest, success, serverError } from '../../helpers/http-helper'
 import { AddCreditOperation } from '../../../domain/usecases/add-operation'
 
 export class OperationController implements Controller {
   constructor (private readonly addCreditOp: AddCreditOperation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['type', 'amount', 'date', 'description']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['type', 'amount', 'date', 'description']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
+      const { type, amount, description, date } = httpRequest.body
+      const creditOperation = await this.addCreditOp.addCreditOperation({ type, amount, description, date })
+      return success(creditOperation)
+    } catch (error) {
+      return serverError(error)
     }
-    const { type, amount, description, date } = httpRequest.body
-    const creditOperation = await this.addCreditOp.addCreditOperation({ type, amount, description, date })
-    return success(creditOperation)
   }
 }
