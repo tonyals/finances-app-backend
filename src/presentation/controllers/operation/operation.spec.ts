34854b1abc07aabeb03dfb4a91611016a5577591
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-05-07 18:20:53 -0300

    test: ensure OperationController calls AddDebitOperation with correct values

--- src/presentation/controllers/operation/operation.spec.ts
+++ src/presentation/controllers/operation/operation.spec.ts
@@ -1,9 +1,10 @@
 import { OperationController } from './operation'
 import { badRequest, success, serverError } from '../../helpers/http-helper'
 import { MissingParamError } from '../../errors/missing-param'
-import { OperationType, OperationCreditModel } from '../../../domain/models/operation-model'
+import { OperationType, OperationCreditModel, OperationDebitModel } from '../../../domain/models/operation-model'
 import { AddCreditOperation, AddCreditOperationModel } from '../../../domain/usecases/add-operation'
 import MockDate from 'mockdate'
+import { AddDebitOperation, AddDebitOperationModel } from '../../../domain/usecases/add-debit-operation'
 
 const makeAddCreditOperation = (): AddCreditOperation => {
   class AddCreditOperationStub implements AddCreditOperation {
@@ -20,15 +21,32 @@ const makeAddCreditOperation = (): AddCreditOperation => {
   return new AddCreditOperationStub()
 }
 
+const makeAddDebitOperation = (): AddDebitOperation => {
+  class AddDebitOperationStub implements AddDebitOperation {
+    async addDebitOperation (operationData: AddDebitOperationModel): Promise<OperationDebitModel> {
+      return new Promise(resolve => resolve({
+        id: 1,
+        type: OperationType.DEBIT,
+        amount: 1,
+        date: new Date(),
+        description: 'any_description'
+      }))
+    }
+  }
+  return new AddDebitOperationStub()
+}
+
 interface SutTypes {
   sut: OperationController
   addCreditOperationStub: AddCreditOperation
+  addDebitOperationStub: AddDebitOperation
 }
 
 const makeSut = (): SutTypes => {
   const addCreditOperationStub = makeAddCreditOperation()
-  const sut = new OperationController(addCreditOperationStub)
-  return { sut, addCreditOperationStub }
+  const addDebitOperationStub = makeAddDebitOperation()
+  const sut = new OperationController(addCreditOperationStub, addDebitOperationStub)
+  return { sut, addCreditOperationStub, addDebitOperationStub }
 }
 
 describe('OperationController', () => {
@@ -51,94 +69,115 @@ describe('OperationController', () => {
     })
     expect(httpResponse).toEqual(badRequest(new MissingParamError('type')))
   })
-
-  test('should return 400 if no amount is provided', async () => {
-    const { sut } = makeSut()
-    const httpResponse = await sut.handle({
-      body: {
-        type: OperationType.CREDIT,
-        date: new Date(),
-        description: 'any_description'
-      }
+  describe('Operation Credit', () => {
+    test('should return 400 if no amount is provided', async () => {
+      const { sut } = makeSut()
+      const httpResponse = await sut.handle({
+        body: {
+          type: OperationType.CREDIT,
+          date: new Date(),
+          description: 'any_description'
+        }
+      })
+      expect(httpResponse).toEqual(badRequest(new MissingParamError('amount')))
     })
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('amount')))
-  })
 
-  test('should return 400 if no date is provided', async () => {
-    const { sut } = makeSut()
-    const httpResponse = await sut.handle({
-      body: {
-        type: OperationType.CREDIT,
-        amount: 1,
-        description: 'any_description'
-      }
+    test('should return 400 if no date is provided', async () => {
+      const { sut } = makeSut()
+      const httpResponse = await sut.handle({
+        body: {
+          type: OperationType.CREDIT,
+          amount: 1,
+          description: 'any_description'
+        }
+      })
+      expect(httpResponse).toEqual(badRequest(new MissingParamError('date')))
     })
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('date')))
-  })
 
-  test('should return 400 if no description is provided', async () => {
-    const { sut } = makeSut()
-    const httpResponse = await sut.handle({
-      body: {
-        type: OperationType.CREDIT,
-        amount: 1,
-        date: new Date()
-      }
+    test('should return 400 if no description is provided', async () => {
+      const { sut } = makeSut()
+      const httpResponse = await sut.handle({
+        body: {
+          type: OperationType.CREDIT,
+          amount: 1,
+          date: new Date()
+        }
+      })
+      expect(httpResponse).toEqual(badRequest(new MissingParamError('description')))
     })
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('description')))
-  })
 
-  test('should call AddCreditOperation with correct values', async () => {
-    const { sut, addCreditOperationStub } = makeSut()
-    const addCreditOperationSpy = jest.spyOn(addCreditOperationStub, 'addCreditOperation')
-    await sut.handle({
-      body: {
-        type: OperationType.CREDIT,
-        amount: 1,
-        date: new Date(),
-        description: 'any_description'
-      }
-    })
-    expect(addCreditOperationSpy).toHaveBeenCalledWith({
-      type: OperationType.CREDIT,
-      amount: 1,
-      date: new Date(),
-      description: 'any_description'
-    })
-  })
-
-  test('should return an creditOperation if addCreditOperation success', async () => {
-    const { sut } = makeSut()
-    const httpResponse = await sut.handle({
-      body: {
+    test('should call AddCreditOperation with correct values', async () => {
+      const { sut, addCreditOperationStub } = makeSut()
+      const addCreditOperationSpy = jest.spyOn(addCreditOperationStub, 'addCreditOperation')
+      await sut.handle({
+        body: {
+          type: OperationType.CREDIT,
+          amount: 1,
+          date: new Date(),
+          description: 'any_description'
+        }
+      })
+      expect(addCreditOperationSpy).toHaveBeenCalledWith({
         type: OperationType.CREDIT,
         amount: 1,
         date: new Date(),
         description: 'any_description'
-      }
+      })
     })
-    expect(httpResponse).toEqual(success({
-      id: 1,
-      type: OperationType.CREDIT,
-      amount: 1,
-      date: new Date(),
-      description: 'any_description'
-    }))
-  })
 
-  test('should return 500 if addCreditOperation throws', async () => {
-    const { sut, addCreditOperationStub } = makeSut()
-    jest.spyOn(addCreditOperationStub, 'addCreditOperation').mockImplementationOnce(() => {
-      throw new Error()
-    })
-    const httpResponse = await sut.handle({
-      body: {
+    test('should return an creditOperation if addCreditOperation success', async () => {
+      const { sut } = makeSut()
+      const httpResponse = await sut.handle({
+        body: {
+          type: OperationType.CREDIT,
+          amount: 1,
+          date: new Date(),
+          description: 'any_description'
+        }
+      })
+      expect(httpResponse).toEqual(success({
+        id: 1,
         type: OperationType.CREDIT,
         amount: 1,
         date: new Date(),
         description: 'any_description'
-      }
+      }))
     })
-    expect(httpResponse).toEqual(serverError(new Error()))
+
+    test('should return 500 if addCreditOperation throws', async () => {
+      const { sut, addCreditOperationStub } = makeSut()
+      jest.spyOn(addCreditOperationStub, 'addCreditOperation').mockImplementationOnce(() => {
+        throw new Error()
+      })
+      const httpResponse = await sut.handle({
+        body: {
+          type: OperationType.CREDIT,
+          amount: 1,
+          date: new Date(),
+          description: 'any_description'
+        }
+      })
+      expect(httpResponse).toEqual(serverError(new Error()))
+    })
+  })
+  describe('Operation Debit', () => {
+    test('should call AddDebitOperation with correct values', async () => {
+      const { sut, addDebitOperationStub } = makeSut()
+      const addDebitOperationSpy = jest.spyOn(addDebitOperationStub, 'addDebitOperation')
+      await sut.handle({
+        body: {
+          type: OperationType.DEBIT,
+          amount: 1,
+          date: new Date(),
+          description: 'any_description'
+        }
+      })
+      expect(addDebitOperationSpy).toHaveBeenCalledWith({
+        type: OperationType.DEBIT,
+        amount: 1,
+        date: new Date(),
+        description: 'any_description'
+      })
+    })
   })
 })
