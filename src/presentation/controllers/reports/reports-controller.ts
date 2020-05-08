ed5fcd5a0a3daefc9a5608fca097344f92dc8590
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-05-08 16:00:53 -0300

    feat: ensure ReportsController returns 400 if invalid type operation is provided

--- src/presentation/controllers/reports/reports-controller.ts
+++ src/presentation/controllers/reports/reports-controller.ts
@@ -1,14 +1,29 @@
 import { Controller } from '../../protocols/controller'
 import { HttpResponse, HttpRequest } from '../../protocols/http'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, success } from '../../helpers/http-helper'
 import { MissingParamError } from '../../errors/missing-param'
+import { OperationType } from '../../../domain/models/operation-enum'
+import { SumAllDebitsOperation } from '../../../domain/usecases/sum-debits'
+import { InvalidParamError } from '../../errors/invalid-param'
 
 export class ReportsController implements Controller {
+  constructor (
+    private readonly sumAllDebits: SumAllDebitsOperation
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { type } = httpRequest.body
     if (!type) {
       return badRequest(new MissingParamError('type'))
     }
-    return new Promise(resolve => resolve(null))
+    switch (type) {
+      case OperationType.DEBIT: {
+        const debitOperation = await this.sumAllDebits
+          .sumAllDebitsOperation(OperationType.DEBIT)
+        return success(debitOperation)
+      }
+      default:
+        return badRequest(new InvalidParamError('operation-type'))
+    }
   }
 }
