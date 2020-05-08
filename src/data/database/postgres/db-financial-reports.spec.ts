a4aa346d942d20eb509f844e96543cadb5119835
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-05-08 20:44:51 -0300

    test: ensure DbFinancialReports call financialResultRepository

--- src/data/database/postgres/db-financial-reports.spec.ts
+++ src/data/database/postgres/db-financial-reports.spec.ts
@@ -2,9 +2,12 @@ import { DbFinancialReportsOperation } from './db-financial-reports'
 import { SumAllOperationRepository } from '../usecases/sum-debits-repository'
 import { OperationType } from '../../../domain/models/operation-enum'
 import { SumAllModel } from '../../../domain/models/sum-all-model'
+import { FinancialResultRepository } from '../usecases/financial-result-repository'
+import { FinancialResultModel } from '../../../domain/models/financial-result-model'
 
 interface SutTypes {
   sut: DbFinancialReportsOperation
+  financialResultRepositoryStub: FinancialResultRepository
   sumAllOpRepositoryStub: SumAllOperationRepository
 }
 
@@ -27,56 +30,80 @@ const makeSumAllOperationRepository = (): SumAllOperationRepository => {
   return new SumAllOperationRepositoryStub()
 }
 
+const makeFinancialResultRepository = (): FinancialResultRepository => {
+  class FinancialResultRepositoryStub implements FinancialResultRepository {
+    async financialResult (): Promise<FinancialResultModel> {
+      return new Promise(resolve => resolve({
+        sumDebits: 25,
+        sumCredits: 50,
+        result: 25
+      }))
+    }
+  }
+  return new FinancialResultRepositoryStub()
+}
+
 const makeSut = (): SutTypes => {
   const sumAllOpRepositoryStub = makeSumAllOperationRepository()
-  const sut = new DbFinancialReportsOperation(sumAllOpRepositoryStub)
-  return { sut, sumAllOpRepositoryStub }
+  const financialResultRepositoryStub = makeFinancialResultRepository()
+  const sut = new DbFinancialReportsOperation(sumAllOpRepositoryStub, financialResultRepositoryStub)
+  return { sut, sumAllOpRepositoryStub, financialResultRepositoryStub }
 }
 
 describe('DbFinancialReports', () => {
-  test('should call sumAllOpRepository with correct value if DEBIT', async () => {
-    const { sut, sumAllOpRepositoryStub } = makeSut()
-    const sumSpy = jest.spyOn(sumAllOpRepositoryStub, 'sumAllOperationRepository')
-    await sut.sumAllOperation(OperationType.DEBIT)
-    expect(sumSpy).toHaveBeenCalledWith('DEBIT')
-  })
+  describe('Sum operations', () => {
+    test('should call sumAllOpRepository with correct value if DEBIT', async () => {
+      const { sut, sumAllOpRepositoryStub } = makeSut()
+      const sumSpy = jest.spyOn(sumAllOpRepositoryStub, 'sumAllOperationRepository')
+      await sut.sumAllOperation(OperationType.DEBIT)
+      expect(sumSpy).toHaveBeenCalledWith('DEBIT')
+    })
 
-  test('should call sumAllOpRepository with correct value if CREDIT', async () => {
-    const { sut, sumAllOpRepositoryStub } = makeSut()
-    const sumSpy = jest.spyOn(sumAllOpRepositoryStub, 'sumAllOperationRepository')
-    await sut.sumAllOperation(OperationType.CREDIT)
-    expect(sumSpy).toHaveBeenCalledWith('CREDIT')
-  })
+    test('should call sumAllOpRepository with correct value if CREDIT', async () => {
+      const { sut, sumAllOpRepositoryStub } = makeSut()
+      const sumSpy = jest.spyOn(sumAllOpRepositoryStub, 'sumAllOperationRepository')
+      await sut.sumAllOperation(OperationType.CREDIT)
+      expect(sumSpy).toHaveBeenCalledWith('CREDIT')
+    })
 
-  test('should sumAllOpRepository returns DEBIT operations on success', async () => {
-    const { sut } = makeSut()
-    const debitOperation = await sut.sumAllOperation(OperationType.DEBIT)
-    expect(debitOperation).toEqual({
-      operation: [
-        {
-          id: 1,
-          type: OperationType.DEBIT,
-          description: 'any_description',
-          amount: 1
-        }
-      ],
-      sum: 2
+    test('should sumAllOpRepository returns DEBIT operations on success', async () => {
+      const { sut } = makeSut()
+      const debitOperation = await sut.sumAllOperation(OperationType.DEBIT)
+      expect(debitOperation).toEqual({
+        operation: [
+          {
+            id: 1,
+            type: OperationType.DEBIT,
+            description: 'any_description',
+            amount: 1
+          }
+        ],
+        sum: 2
+      })
+    })
+
+    test('should sumAllOpRepository returns CREDIT operations on success', async () => {
+      const { sut } = makeSut()
+      const creditOperation = await sut.sumAllOperation(OperationType.CREDIT)
+      expect(creditOperation).toEqual({
+        operation: [
+          {
+            id: 1,
+            type: OperationType.CREDIT,
+            description: 'any_description',
+            amount: 1
+          }
+        ],
+        sum: 2
+      })
     })
   })
-
-  test('should sumAllOpRepository returns CREDIT operations on success', async () => {
-    const { sut } = makeSut()
-    const creditOperation = await sut.sumAllOperation(OperationType.CREDIT)
-    expect(creditOperation).toEqual({
-      operation: [
-        {
-          id: 1,
-          type: OperationType.CREDIT,
-          description: 'any_description',
-          amount: 1
-        }
-      ],
-      sum: 2
+  describe('Financial Results operations', () => {
+    test('should call financialResult', async () => {
+      const { sut, financialResultRepositoryStub } = makeSut()
+      const financialResultSpy = jest.spyOn(financialResultRepositoryStub, 'financialResult')
+      await sut.financialResult()
+      expect(financialResultSpy).toHaveBeenCalled()
     })
   })
 })
