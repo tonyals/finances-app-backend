16ec16ed9701a663b2d3a0150188361daa6c3779
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-05-08 20:44:11 -0300

    feat: ensure DbFinancialReports call financialResultRepository

--- src/data/database/postgres/db-financial-reports.ts
+++ src/data/database/postgres/db-financial-reports.ts
@@ -2,14 +2,23 @@ import { SumAllOperation } from '../../../domain/usecases/sum-all'
 import { OperationType } from '../../../domain/models/operation-enum'
 import { SumAllModel } from '../../../domain/models/sum-all-model'
 import { SumAllOperationRepository } from '../usecases/sum-debits-repository'
+import { FinancialResultOperation } from '../../../domain/usecases/financial-result'
+import { FinancialResultModel } from '../../../domain/models/financial-result-model'
+import { FinancialResultRepository } from '../usecases/financial-result-repository'
 
-export class DbFinancialReportsOperation implements SumAllOperation {
+export class DbFinancialReportsOperation implements SumAllOperation, FinancialResultOperation {
   constructor (
-    private readonly sumAllOpRepository: SumAllOperationRepository
+    private readonly sumAllOpRepository: SumAllOperationRepository,
+    private readonly financialResultOpRepository: FinancialResultRepository
   ) {}
 
   async sumAllOperation (type: OperationType): Promise<SumAllModel> {
     const operation = await this.sumAllOpRepository.sumAllOperationRepository(type)
     return operation
+  }
+
+  async financialResult (): Promise<FinancialResultModel> {
+    await this.financialResultOpRepository.financialResult()
+    return new Promise(resolve => resolve(null))
   }
 }
