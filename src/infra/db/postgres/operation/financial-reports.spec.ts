d9f46ddee8664b587a5f9c2de350a5e8c0470dfa
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-05-08 19:10:46 -0300

    test: ensure FinancialReportsPostgresRepository returns a array credit operations on success

--- src/infra/db/postgres/operation/financial-reports.spec.ts
+++ src/infra/db/postgres/operation/financial-reports.spec.ts
@@ -7,17 +7,16 @@ import { OperationType } from '../../../../domain/models/operation-enum'
 describe('Operation Repository', () => {
   beforeAll(async () => {
     await CreateConnectionPostgres.connect()
-  })
-  beforeEach(async () => {
     await Operation.create({
       type: OperationType.DEBIT,
       amount: 2.85,
       date: new Date(),
       description: 'any_description'
     }).save()
+
     await Operation.create({
-      type: OperationType.DEBIT,
-      amount: 3.5,
+      type: OperationType.CREDIT,
+      amount: 2.95,
       date: new Date(),
       description: 'any_description'
     }).save()
@@ -32,5 +31,15 @@ describe('Operation Repository', () => {
     const sut = new FinancialReportsPostgresRepository()
     const operationDebit = await sut.sumAllOperationRepository(OperationType.DEBIT)
     expect(operationDebit).toBeTruthy()
+    expect(operationDebit.operation[0]).toBeTruthy()
+    expect(operationDebit.sum).toBe(2.85)
+  })
+
+  test('should return an array of CreditOperation if success', async () => {
+    const sut = new FinancialReportsPostgresRepository()
+    const operationCredit = await sut.sumAllOperationRepository(OperationType.CREDIT)
+    expect(operationCredit).toBeTruthy()
+    expect(operationCredit.operation[0]).toBeTruthy()
+    expect(operationCredit.sum).toBe(2.95)
   })
 })
