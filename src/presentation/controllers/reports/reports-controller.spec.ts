84ca714bfa82cb3dd0d7115fa6f3d61ca51a4f99
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-05-08 16:01:18 -0300

    test: ensure ReportsController returns 400 if invalid type operation is provided

--- src/presentation/controllers/reports/reports-controller.spec.ts
+++ src/presentation/controllers/reports/reports-controller.spec.ts
@@ -1,14 +1,40 @@
 import { ReportsController } from './reports-controller'
 import { badRequest } from '../../helpers/http-helper'
 import { MissingParamError } from '../../errors/missing-param'
+import { SumAllDebitsOperation } from '../../../domain/usecases/sum-debits'
+import { SumAllDebitsModel } from '../../../domain/models/sum-debits-model'
+import { OperationType } from '../../../domain/models/operation-enum'
+import { InvalidParamError } from '../../errors/invalid-param'
 
 interface SutTypes {
   sut: ReportsController
+  sumAllDebitsStub: SumAllDebitsOperation
+}
+
+const makeSumAllDebitsOperation = (): SumAllDebitsOperation => {
+  class SumAllDebitsOperationStub implements SumAllDebitsOperation {
+    async sumAllDebitsOperation (type: OperationType.DEBIT): Promise<SumAllDebitsModel> {
+      return new Promise(resolve => resolve({
+        debits: [
+          {
+            id: 1,
+            type: OperationType.DEBIT,
+            description: 'any_description',
+            amount: 1
+          }
+        ],
+        sumDebits: 2
+
+      }))
+    }
+  }
+  return new SumAllDebitsOperationStub()
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new ReportsController()
-  return { sut }
+  const sumAllDebitsStub = makeSumAllDebitsOperation()
+  const sut = new ReportsController(sumAllDebitsStub)
+  return { sut, sumAllDebitsStub }
 }
 
 describe('ReportsController', () => {
@@ -18,5 +44,15 @@ describe('ReportsController', () => {
       body: {}
     })
     expect(httpResponse).toEqual(badRequest(new MissingParamError('type')))
+  })
+
+  test('should return 400 if invalid type operation is provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({
+      body: {
+        type: 'any_operation'
+      }
+    })
+    expect(httpResponse).toEqual(badRequest(new InvalidParamError('operation-type')))
   })
 })
