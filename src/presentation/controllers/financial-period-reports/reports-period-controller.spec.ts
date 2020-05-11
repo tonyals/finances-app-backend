8a6d7af4235421c8991b15a52ea5b3c9da226183
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-05-10 22:17:02 -0300

    test: ensure ReportsPeriodController returns 400 if no type report is provided

---
+++ src/presentation/controllers/financial-period-reports/reports-period-controller.spec.ts
@@ -0,0 +1,66 @@
+import { FinancialPeriodReportsController } from './reports-period-controller'
+import { badRequest } from '../../helpers/http-helper'
+import { MissingParamError } from '../../errors/missing-param'
+import { SumModel } from '../../../domain/models/sum-model'
+import { OperationType } from '../../../domain/models/operation-enum'
+import { SumPeriodOperation, Period } from '../../../domain/usecases/sum-period'
+
+interface SutTypes {
+  sut: FinancialPeriodReportsController
+  sumPeriodStub: SumPeriodOperation
+}
+
+const makeSumPeriodOperation = (): SumPeriodOperation => {
+  class SumPeriodOperationStub implements SumPeriodOperation {
+    async sumPeriodOperation (type: OperationType, period: Period): Promise<SumModel> {
+      return new Promise(resolve => resolve({
+        operation: [
+          {
+            id: 1,
+            type: type,
+            date: new Date('2020-05-10'),
+            period: {
+              initialDate: period.initialDate,
+              finalDate: period.finalDate
+            },
+            description: 'any_description',
+            amount: 2
+          },
+          {
+            id: 2,
+            type: type,
+            date: new Date('2020-05-10'),
+            period: {
+              initialDate: period.initialDate,
+              finalDate: period.finalDate
+            },
+            description: 'any_description',
+            amount: 3
+          }
+        ],
+        sum: 5
+      }))
+    }
+  }
+  return new SumPeriodOperationStub()
+}
+
+const makeSut = (): SutTypes => {
+  const sumPeriodStub = makeSumPeriodOperation()
+  const sut = new FinancialPeriodReportsController(sumPeriodStub)
+  return { sut, sumPeriodStub }
+}
+
+describe('ReportsController', () => {
+  test('should return 400 if no type report is provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({
+      body: {
+        operation: 'any-operation',
+        initialDate: 'any-date',
+        finalDate: 'any-date'
+      }
+    })
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('type-report')))
+  })
+})
